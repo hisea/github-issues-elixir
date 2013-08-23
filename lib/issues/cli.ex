820378d6f020ac0c83e3f46a20dff5f73b7d3c2e
Hisea <zyinghai@gmail.com>
2013-08-22 23:49:44 -0400

    Continue to work on the tutorial.

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -11,7 +11,9 @@ defmodule Issues.CLI do
 
 
   def run(argv) do
-    parse_args(argv)
+    argv
+      |> parse_args
+      |> process
   end
 
   @doc """
@@ -31,5 +33,37 @@ defmodule Issues.CLI do
     { _, [ user, project ] }        -> { user, project, @default_count }
     _                               -> :help
     end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+      |> decode_response
+      |> convert_to_list_of_hashdicts
+      |> sort_into_ascending_order
+      |> Enum.take(count)
+      |> Issues.TableFormatter.print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def decode_response({:ok, body}), do: Jsonex.decode(body)
+  def decode_response({:error, msg}) do
+    error = Jsonex.decode(msg)["message"]
+    IO.puts "Error fetching from Github: #{error}"
+    System.halt(2)
+  end
+
+  def convert_to_list_of_hashdicts(list) do
+    list |> Enum.map(&HashDict.new/1)
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues,
+      fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
   end
 end
